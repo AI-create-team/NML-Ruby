e7f11edbec85c385718e0927f11711ff1f4e85b6
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-03-07 21:52:06 +0900

    ルビ実装

--- Main.rb
+++ Main.rb
@@ -1,3 +1,4 @@
+
 require 'bundler/setup'
 Bundler.require
 require 'sinatra'
@@ -13,20 +14,41 @@ post '/' do
 
   s = String.new
 
-  spstr.each do |youso|
-    youso = checkSpace(youso)
-    youso << "\n"
+  spstr.each do |line|
+    line = checkSpace(line)
+    checkRuby(line)
+    line << "\n"
 
-    s << youso
+    s << line
   end
 
   return s[0,s.length - 1]
 
 end
 
-def checkSpace(youso)
-  if youso.match(/^[ \s]/) then
-    youso = "<p>" + youso + "</p>"
+def checkSpace(line)
+  if line.match(/^[ \s]/) then
+    line = "<p>" + line + "</p>"
   end
-  return youso
+  return line
+end
+
+def checkRuby(line)
+
+  s = line.force_encoding("UTF-8").scan(/[\|｜].*?[\(（].*?[\)）]/)
+
+  s.each do |text|
+    moji = text[/[\|｜].*?[\(（]/]
+    moji = moji[1,moji.length-2]
+
+    ruby = text[/[\(（].*?[\)）]/]
+    ruby = ruby[1,ruby.length-2]
+
+    s = "<ruby>" + moji + "<rt>" + ruby + "</rt></ruby>"
+
+    line.sub!(text, s)
+  end
+
+  return line
+
 end
