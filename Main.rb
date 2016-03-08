c9fb8b474fe608db5f4f07681d8c36092083e964
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-03-08 19:53:00 +0900

    斜体/太字の修正

--- Main.rb
+++ Main.rb
@@ -129,7 +129,7 @@ end
 
 #斜体
 def checkItalic(line)
-  s = line.force_encoding("UTF-8").scan(/[\_＿]{1}.*?[\_＿]{1}/)
+  s = line.force_encoding("UTF-8").scan(/[\_＿*＊]{1}.*?[\_＿*＊]{1}/)
   s.each do |text|
     if text != "__"
       ss = "<i>#{text[1,text.length - 2]}</i>"
@@ -142,7 +142,7 @@ end
 
 #太字
 def checkBold(line)
-  s = line.force_encoding("UTF-8").scan(/[\_＿]{2}.*?[\_＿]{2}/)
+  s = line.force_encoding("UTF-8").scan(/[\_＿*＊]{2}.*?[\_＿*＊]{2}/)
   s.each do |text|
     line.gsub!(text, "<b>#{text[2,text.length - 4]}</b>")
   end
