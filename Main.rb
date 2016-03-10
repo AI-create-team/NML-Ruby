61ddbfd6a646823ea623acd9f881988c08cfbae9
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-03-10 22:18:01 +0900

    正規表現に修正

--- Main.rb
+++ Main.rb
@@ -11,7 +11,7 @@ post '/' do
 
   body = request.body.read.force_encoding('UTF-8').split("\n")
   body.each do |line|
-    line.gsub!("\n","")
+    line.gsub!(/\n/,"")
     s << checkLine(line) << "\n"
   end
 
